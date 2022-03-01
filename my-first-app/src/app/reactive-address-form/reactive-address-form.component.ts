bc7d9498ecdc3463ec61e3f0a2e2c13477413b6f
Naynesh335 <nayneshchaudhary@gmail.com>
2022-03-01 11:49:12 +0530

    task Completed : reactive form

--- my-first-app/src/app/reactive-address-form/reactive-address-form.component.ts
+++ my-first-app/src/app/reactive-address-form/reactive-address-form.component.ts
@@ -19,7 +19,9 @@ export class ReactiveAddressFormComponent implements OnInit {
       email:['', [Validators.required,Validators.email]],
       phonenum: ['', [Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
       city:['',Validators.required],
-      age:[ ,[Validators.required,Validators.min(18)]]
+      age:[ ,[Validators.required,Validators.min(18)]],
+      sdate: ['',Validators.required],
+      edate: ['',Validators.required]
     });
   }
 
@@ -30,6 +32,14 @@ export class ReactiveAddressFormComponent implements OnInit {
     console.log('Phone Number', form.value.phonenum);
     console.log('City', form.value.city);
     console.log('Age', form.value.age);
+    console.log("Days ", (Math.ceil(Math.abs(this.FindDays(form.value.sdate) - this.FindDays(form.value.edate)))/(1000 * 3600 * 24)));
+  }
+
+  d : any;
+  FindDays(form:any)
+  {
+    this.d=new Date(form) 
+    return this.d.getTime();
   }
 
 }
